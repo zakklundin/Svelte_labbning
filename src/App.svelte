b3449700a45e17aba1df3bbed935e1f4213ecec3
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2021-05-24 13:30:55 +0200

    result buttons now appear after first one is pressed

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,63 @@
 <script>
+import { insert } from "svelte/internal";
+
+
 	export let inputNumber = 0;
 	let resultNumber = 0
-	let cubicM = false
-	let squareM = false
-	let litre = false
 	let selected = 'Choose below'
 	let selected2 = 'Choose below'
+
+	function insertButton(div, name, text, multiplier){
+		name = document.createElement("button")
+		name.innerHTML = text
+		name.onclick = function(){
+			resultNumber = inputNumber * multiplier
+			selected2 = name
+		}
+		div.appendChild(name)
+	}
 
 	function fromCubM(){
 		cubicM = true
 		squareM = false
 		litre = false
 		selected = 'Cubic meter(s)'
+		let toUnit = document.getElementById("toUnit")
+		toUnit.innerHTML = ""
+		let btn1 = "toCubicCM"
+		let btn2 = "toLitre"
+		let btn3 = "toCubicMM"
+		insertButton(toUnit, btn1, "Cubic Centimeters", 1000000)
+		insertButton(toUnit, btn2, "Litres", 1000)
+		insertButton(toUnit, btn3, "Cubic Millimeters", 1000000000)
 	}
 	function fromSqM(){
 		squareM = true
 		cubicM = false
 		litre = false
 		selected = 'Square meter(s)'
+		let toUnit = document.getElementById("toUnit")
+		toUnit.innerHTML = ""
+		let btn1 = "toSqCM"
+		let btn2 = "toHA"
+		insertButton(toUnit, btn1, "Square Centimeters", 10000)
+		insertButton(toUnit, btn2, "Hectares", 0.0001)
 	}
 	function fromLitre(){
 		litre = true
 		squareM = false
 		cubicM = false
 		selected = 'Litre(s)'
+		let toUnit = document.getElementById("toUnit")
+		toUnit.innerHTML = ""
+		let btn1 = "toCubicCM"
+		let btn2 = "toLitre"
+		let btn3 = "toCubicMM"
+		insertButton(toUnit, btn1, "Cubic Centimeters", 1000)
+		insertButton(toUnit, btn2, "Litres", 1)
+		insertButton(toUnit, btn3, "Cubic Millimeters", 1000000)
 	}
-
+/*
 	function toSqCM(){
 		if (cubicM == true || litre == true) {
 			resultNumber = "ERROR, incompatible units"
@@ -45,11 +77,12 @@
 		}
 		selected2 = 'Cubic Centimeter(s)'
 	}
+
 	function toLitre() {
 		if (squareM == true){
 			resultNumber = "ERROR, incompatible units"
 		} else if (cubicM == true) {
-			resultNumber = inputNumber * 0.001
+			resultNumber = inputNumber * 1000
 		} else if (litre == true) {
 			resultNumber = inputNumber
 		}
@@ -73,7 +106,7 @@
 		}
 		selected2 = "Cubic Millimeter(s)"
 	}
-
+*/
 </script>
 
 <main>
@@ -87,12 +120,11 @@
 	<button id="Litre" on:click={fromLitre}>Litres</button>
 	
 	<p>To: {selected2}</p>
-	<button id="SqCM" on:click={toSqCM}>Square Centimeters</button>
-	<button on:click={toHA}>Hectare</button>
-	<button id="CubeCM" on:click={toCubCM}>Cubic Centimeters</button>
-	<button on:click={toLitre}>Litres</button>
-	<button on:click={toCubMM}>Cubic Millimeter</button>
 
+	<div id="toUnit">
+
+	</div>
+	
 	<p>Result: {resultNumber}</p>
 </main>
 
